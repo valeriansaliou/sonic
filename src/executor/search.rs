2b40fa35661815c2b6094629674343d37c151fc3
Valerian Saliou <valerian@valeriansaliou.name>
2019-03-14 16:59:14 +0100

    closes #9
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/executor/search.rs
+++ src/executor/search.rs
@@ -9,7 +9,8 @@ use std::iter::FromIterator;
 
 use crate::lexer::token::TokenLexer;
 use crate::query::types::{QuerySearchID, QuerySearchLimit, QuerySearchOffset};
-use crate::store::identifiers::StoreObjectIID;
+use crate::store::fst::{StoreFSTActionBuilder, StoreFSTPool, GRAPH_ACCESS_LOCK};
+use crate::store::identifiers::{StoreObjectIID, StoreTermHash};
 use crate::store::item::StoreItem;
 use crate::store::kv::{StoreKVActionBuilder, StoreKVPool, STORE_ACCESS_LOCK};
 
@@ -26,51 +27,78 @@ impl ExecutorSearch {
         if let StoreItem(collection, Some(bucket), None) = store {
             // Important: acquire database access read lock, and reference it in context. This \
             //   prevents the database from being erased while using it in this block.
-            let _kv_access = STORE_ACCESS_LOCK.read().unwrap();
+            let (_kv_access, _fst_access) = (
+                STORE_ACCESS_LOCK.read().unwrap(),
+                GRAPH_ACCESS_LOCK.read().unwrap(),
+            );
 
-            if let Ok(kv_store) = StoreKVPool::acquire(collection) {
-                let kv_action = StoreKVActionBuilder::read(bucket, kv_store);
-
-                // TODO: auto-correct invalid words from FST w/ auto-complete + typo-correct \
-                //   via levenshtein distances.
+            if let (Ok(kv_store), Ok(fst_store)) = (
+                StoreKVPool::acquire(collection),
+                StoreFSTPool::acquire(collection, bucket),
+            ) {
+                let (kv_action, fst_action) = (
+                    StoreKVActionBuilder::read(bucket, kv_store),
+                    StoreFSTActionBuilder::read(fst_store),
+                );
 
                 // Try to resolve existing search terms to IIDs, and perform an algebraic AND on \
                 //   all resulting IIDs for each given term.
                 let mut found_iids: LinkedHashSet<StoreObjectIID> = LinkedHashSet::new();
 
                 while let Some((term, term_hashed)) = lexer.next() {
-                    if let Ok(iids_inner) = kv_action.get_term_to_iids(term_hashed) {
-                        let iids = iids_inner.unwrap_or(Vec::new());
+                    let mut iids = kv_action
+                        .get_term_to_iids(term_hashed)
+                        .unwrap_or(None)
+                        .unwrap_or(Vec::new());
 
-                        debug!("got search executor iids: {:?} for term: {}", iids, term);
+                    // No IIDs? Try to complete with a suggested alternate word
+                    if iids.is_empty() == true {
+                        debug!("no iid was found, completing for term: {}", term);
 
-                        // Intersect found IIDs with previous batch
-                        let iids_set: LinkedHashSet<StoreObjectIID> =
-                            LinkedHashSet::from_iter(iids.into_iter());
+                        if let Some(suggested_words) = fst_action.suggest_words(&term, 1) {
+                            if let Some(suggested_word) = suggested_words.first() {
+                                debug!("got completed word: {} for term: {}", suggested_word, term);
 
-                        if found_iids.is_empty() == true {
-                            found_iids = iids_set;
+                                if let Some(suggested_iids) = kv_action
+                                    .get_term_to_iids(StoreTermHash::from(&suggested_word))
+                                    .unwrap_or(None)
+                                {
+                                    iids.extend(suggested_iids);
+                                }
+                            }
                         } else {
-                            found_iids = found_iids
-                                .intersection(&iids_set)
-                                .map(|value| *value)
-                                .collect();
+                            debug!("did not get any completed word for term: {}", term);
                         }
+                    }
 
-                        debug!(
-                            "got search executor iid intersection: {:?} for term: {}",
-                            found_iids, term
+                    debug!("got search executor iids: {:?} for term: {}", iids, term);
+
+                    // Intersect found IIDs with previous batch
+                    let iids_set: LinkedHashSet<StoreObjectIID> =
+                        LinkedHashSet::from_iter(iids.into_iter());
+
+                    if found_iids.is_empty() == true {
+                        found_iids = iids_set;
+                    } else {
+                        found_iids = found_iids
+                            .intersection(&iids_set)
+                            .map(|value| *value)
+                            .collect();
+                    }
+
+                    debug!(
+                        "got search executor iid intersection: {:?} for term: {}",
+                        found_iids, term
+                    );
+
+                    // No IID found? (stop there)
+                    if found_iids.is_empty() == true {
+                        info!(
+                            "stop search executor as no iid was found in common for term: {}",
+                            term
                         );
 
-                        // No IID found? (stop there)
-                        if found_iids.is_empty() == true {
-                            info!(
-                                "stop search executor as no iid was found in common for term: {}",
-                                term
-                            );
-
-                            break;
-                        }
+                        break;
                     }
                 }
 
