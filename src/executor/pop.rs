30a2dc8ed4fa10f803c5fe66fc180d77ca1f62e5
Raminder Singh <romi_ssk@yahoo.co.in>
2019-04-06 17:16:30 +0530

    Replaced len() == 0 with is_empty()
    See this clippy lint for details:
    https://rust-lang.github.io/rust-clippy/master/index.html#len_zero

--- src/executor/pop.rs
+++ src/executor/pop.rs
@@ -72,7 +72,7 @@ impl ExecutorPop {
                             count_popped = (iid_terms_hashed.len() - remaining_terms.len()) as u32;
 
                             if count_popped > 0 {
-                                if remaining_terms.len() == 0 {
+                                if remaining_terms.is_empty() {
                                     info!("nuke whole bucket for pop executor");
 
                                     // Flush bucket (batch operation, as it is shared w/ other \
