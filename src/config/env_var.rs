ea3cae720b4b84ce42f64fb460e36115f78375bc
Valerian Saliou <valerian@valeriansaliou.name>
2019-06-03 18:50:06 +0200

    Code style normalization
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/config/env_var.rs
+++ src/config/env_var.rs
@@ -12,72 +12,69 @@ use std::path::PathBuf;
 #[derive(Deserialize, PartialEq)]
 struct WrappedString(String);
 
-// deserialize String
-pub fn str<'de, D>(d: D) -> Result<String, D::Error>
+pub fn str<'de, D>(deserializer: D) -> Result<String, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let val = String::deserialize(d)?;
-    match self::is_env_var(&val) {
-        true => Ok(self::get_env_var(&val)),
-        false => Ok(val),
+    let value = String::deserialize(deserializer)?;
+
+    match self::is_env_var(&value) {
+        true => Ok(self::get_env_var(&value)),
+        false => Ok(value),
     }
 }
 
-// deserialize wrapped Option<String>
-pub fn opt_str<'de, D>(d: D) -> Result<Option<String>, D::Error>
+pub fn opt_str<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
 where
     D: Deserializer<'de>,
 {
-    Option::<WrappedString>::deserialize(d).map(|opt: Option<WrappedString>| {
-        opt.map(|x: WrappedString| {
-            let val = x.0;
-            match self::is_env_var(&val) {
-                true => self::get_env_var(&val),
-                false => val,
+    Option::<WrappedString>::deserialize(deserializer).map(|option: Option<WrappedString>| {
+        option.map(|wrapped: WrappedString| {
+            let value = wrapped.0;
+
+            match self::is_env_var(&value) {
+                true => self::get_env_var(&value),
+                false => value,
             }
         })
     })
 }
 
-// deserialize SocketAddr
-// It deserializes visitor as a String and after that it parses it to SocketAddr
-pub fn socket_addr<'de, D>(d: D) -> Result<SocketAddr, D::Error>
+pub fn socket_addr<'de, D>(deserializer: D) -> Result<SocketAddr, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let val = String::deserialize(d)?;
-    match self::is_env_var(&val) {
-        true => Ok(self::get_env_var(&val).parse().unwrap()),
-        false => Ok(val.parse().unwrap()),
+    let value = String::deserialize(deserializer)?;
+
+    match self::is_env_var(&value) {
+        true => Ok(self::get_env_var(&value).parse().unwrap()),
+        false => Ok(value.parse().unwrap()),
     }
 }
 
-// deserialize PathBuf
-// It deserializes visitor as a String and after that it parses it to PathBuf
-pub fn path_buf<'de, D>(d: D) -> Result<PathBuf, D::Error>
+pub fn path_buf<'de, D>(deserializer: D) -> Result<PathBuf, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let val = String::deserialize(d)?;
-    match self::is_env_var(&val) {
-        true => Ok(PathBuf::from(self::get_env_var(&val))),
-        false => Ok(PathBuf::from(val)),
+    let value = String::deserialize(deserializer)?;
+
+    match self::is_env_var(&value) {
+        true => Ok(PathBuf::from(self::get_env_var(&value))),
+        false => Ok(PathBuf::from(value)),
     }
 }
 
-// check using regex if provided visitor contains env variable
-// pattern - ${env.VARIABLE}
-fn is_env_var(s: &str) -> bool {
-    let re = Regex::new(r"^\$\{env\.\w+\}$").expect("env_var: regex is invalid");
-    re.is_match(s)
+fn is_env_var(value: &str) -> bool {
+    Regex::new(r"^\$\{env\.\w+\}$")
+        .expect("env_var: regex is invalid")
+        .is_match(value)
 }
 
-// parses visitor to varaible key and read variable using std::env
 fn get_env_var(wrapped_key: &str) -> String {
     let key: String = String::from(wrapped_key)
         .drain(6..(wrapped_key.len() - 1))
         .collect();
+
     std::env::var(key.clone()).expect(&format!("env_var: variable '{}' is not set", key))
 }
 
@@ -86,7 +83,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn checks_if_visitor_contains_env_var_pattern() {
+    fn it_checks_environment_variable_patterns() {
         assert_eq!(self::is_env_var("${env.XXX}"), true);
         assert_eq!(self::is_env_var("${env.XXX"), false);
         assert_eq!(self::is_env_var("${env.XXX}a"), false);
@@ -99,9 +96,11 @@ mod tests {
     }
 
     #[test]
-    fn get_env_variable() {
+    fn it_gets_environment_variable() {
         std::env::set_var("TEST", "test");
+
         assert_eq!(self::get_env_var("${env.TEST}"), "test");
+
         std::env::remove_var("TEST");
     }
 }
