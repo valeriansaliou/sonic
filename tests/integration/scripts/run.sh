8e40e5d4367f581b4cfea52d26b3d7d1a7e1767b
Valerian Saliou <valerian@valeriansaliou.name>
2019-07-08 09:01:59 +0200

    Fix integration tests script
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- tests/integration/scripts/run.sh
+++ tests/integration/scripts/run.sh
@@ -8,21 +8,44 @@
 #  License: Mozilla Public License v2.0 (MPL v2.0)
 ##
 
-npm ci
-cargo build
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+TESTSPATH="$ABSPATH/../"
+
 STATUS=0
 
-for i in $(find ./scenarios/ -name "*.js")
-do
-    [[ -d ./data/ ]] && rm -r ./data/
-    cargo run -- --config config.cfg &
-    SONIC_PID=$!
-    sleep 2
-    node $i
-    [[ $? -eq 0 ]] || STATUS=1
-    kill $SONIC_PID
-    wait $SONIC_PID
-done
+# Build Sonic
+cargo build
 
-[[ -d ./data/ ]] && rm -r ./data/
+# Run tests
+pushd "$TESTSPATH" > /dev/null
+  # Install test dependencies from a clean state
+  pushd "./runner/" > /dev/null
+    npm ci
+  popd
+
+  # Run each test scenario
+  for scenario in $(find ./scenarios/ -name "*.js")
+  do
+      [[ -d ./instance/data/ ]] && rm -r ./instance/data/
+
+      # Run sonic from a clean state
+      pushd "./instance/" > /dev/null
+        cargo run -- --config config.cfg &
+        SONIC_PID=$!
+        sleep 2
+      popd
+
+      # Run scenario
+      node $scenario
+
+      [[ $? -eq 0 ]] || STATUS=1
+
+      # Stop Sonic
+      kill $SONIC_PID
+      wait $SONIC_PID
+  done
+
+  [[ -d ./instance/data/ ]] && rm -r ./instance/data/
+popd
+
 exit $STATUS
