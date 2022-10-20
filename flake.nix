1024331de323fec6c47cb35123e9898b964e6357
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-10-20 12:39:21 +0300

    nix: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      inherit (builtins) fromTOML readFile substring;
+
+      cargoToml = fromTOML (readFile ./Cargo.toml);
+      version = "${cargoToml.package.version}+${substring 0 8 self.lastModifiedDate}_${self.shortRev or "dirty"}";
+
+      mkSonicServer = { lib, rustPlatform, llvmPackages, clang, ... }:
+        rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          inherit version;
+
+          src = lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          # TODO: fix and enable tests
+          doCheck = false;
+
+          nativeBuildInputs = [
+            llvmPackages.libclang
+            llvmPackages.libcxxClang
+            clang
+          ];
+          # Needed so bindgen can find libclang.so
+          LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion clang}/include";
+
+          postInstall = ''
+            mkdir -p $out/share
+            cp config.cfg $out/share
+          '';
+        };
+
+    in
+    {
+      overlays.sonic-server = final: prev: {
+        sonic-server = prev.callPackage mkSonicServer { };
+      };
+      overlays.default = self.overlays.sonic-server;
+    }
+    //
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        sonic-server = pkgs.callPackage mkSonicServer { };
+      in
+      {
+        packages = {
+          inherit sonic-server;
+          default = sonic-server;
+        };
+
+        apps.sonic-server = {
+          type = "app";
+          program = "${sonic-server}/bin/sonic";
+        };
+        apps.default = self.apps.${system}.sonic-server;
+
+        devShells.default = pkgs.mkShell {
+          inherit (sonic-server) nativeBuildInputs LIBCLANG_PATH BINDGEN_EXTRA_CLANG_ARGS;
+          packages = with pkgs; [ cargo rustc rustfmt clippy rust-analyzer ];
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        };
+      });
+}
