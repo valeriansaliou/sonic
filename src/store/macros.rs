129a9c98d593f393b8f6c3b6d00a744f41eb18ac
Rémi Bardon <remi@remibardon.name>
2026-05-14 20:32:44 +0200

    Remove useless `#[macro_export]`s

--- src/store/macros.rs
+++ src/store/macros.rs
@@ -4,7 +4,6 @@
 // Copyright: 2019, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
-#[macro_export]
 macro_rules! io_error {
     ($error:expr) => {
         io::Error::new(io::ErrorKind::Other, $error)
