9aab8881a2b141c9c0a21ed3ffa89ff1ecdb74c2
Rémi Bardon <remi@remibardon.name>
2026-05-15 10:29:38 +0200

    tools: Standardize formatting of shell scripts
    - Two spaces is what people are used to, because shell scripts can nest
      a lot. This is what `tests/integration/scripts/run.sh` was using
      already by the way (inconsistent with `scripts/*`).
    - `function foo_example` is rarely used, in favor of `foo_example()`.
    - Unify `; do` style.

--- scripts/release_binaries.sh
+++ scripts/release_binaries.sh
@@ -10,21 +10,21 @@
 
 # Read arguments
 while [ "$1" != "" ]; do
-    argument_key=`echo $1 | awk -F= '{print $1}'`
-    argument_value=`echo $1 | awk -F= '{print $2}'`
+  argument_key=`echo $1 | awk -F= '{print $1}'`
+  argument_value=`echo $1 | awk -F= '{print $2}'`
 
-    case $argument_key in
-        -v | --version)
-            # Notice: strip any leading 'v' to the version number
-            SONIC_VERSION="${argument_value/v}"
-            ;;
-        *)
-            echo "Unknown argument received: '$argument_key'"
-            exit 1
-            ;;
-    esac
+  case $argument_key in
+    -v | --version)
+      # Notice: strip any leading 'v' to the version number
+      SONIC_VERSION="${argument_value/v}"
+      ;;
+    *)
+      echo "Unknown argument received: '$argument_key'"
+      exit 1
+      ;;
+  esac
 
-    shift
+  shift
 done
 
 # Ensure release version is provided
@@ -35,23 +35,23 @@ if [ -z "$SONIC_VERSION" ]; then
 fi
 
 # Define release pipeline
-function release_for_architecture {
-    final_tar="v$SONIC_VERSION-$1-$2.tar.gz"
+release_for_architecture() {
+  final_tar="v$SONIC_VERSION-$1-$2.tar.gz"
 
-    rm -rf ./sonic/ && \
-        cargo build --target "$3" --locked --release && \
-        mkdir ./sonic && \
-        cp -p "target/$3/release/sonic" ./sonic/ && \
-        cp -r ./config.cfg sonic/ && \
-        tar --owner=0 --group=0 -czvf "$final_tar" ./sonic && \
-        rm -r ./sonic/
-    release_result=$?
+  rm -rf ./sonic/ && \
+  cargo build --target "$3" --locked --release && \
+  mkdir ./sonic && \
+  cp -p "target/$3/release/sonic" ./sonic/ && \
+  cp -r ./config.cfg sonic/ && \
+  tar --owner=0 --group=0 -czvf "$final_tar" ./sonic && \
+  rm -r ./sonic/
+  release_result=$?
 
-    if [ $release_result -eq 0 ]; then
-        echo "Result: Packed architecture: $1 ($2) to file: $final_tar"
-    fi
+  if [ $release_result -eq 0 ]; then
+    echo "Result: Packed architecture: $1 ($2) to file: $final_tar"
+  fi
 
-    return $release_result
+  return $release_result
 }
 
 # Run release tasks
@@ -61,16 +61,16 @@ BASE_DIR="$ABSPATH/../"
 rc=0
 
 pushd "$BASE_DIR" > /dev/null
-    echo "Executing release steps for Sonic v$SONIC_VERSION..."
+  echo "Executing release steps for Sonic v$SONIC_VERSION..."
 
-    release_for_architecture "x86_64" "gnu" "x86_64-unknown-linux-gnu"
-    rc=$?
+  release_for_architecture "x86_64" "gnu" "x86_64-unknown-linux-gnu"
+  rc=$?
 
-    if [ $rc -eq 0 ]; then
-        echo "Success: Done executing release steps for Sonic v$SONIC_VERSION"
-    else
-        echo "Error: Failed executing release steps for Sonic v$SONIC_VERSION"
-    fi
+  if [ $rc -eq 0 ]; then
+    echo "Success: Done executing release steps for Sonic v$SONIC_VERSION"
+  else
+    echo "Error: Failed executing release steps for Sonic v$SONIC_VERSION"
+  fi
 popd > /dev/null
 
 exit $rc
