fe3d2cce85128ea2eedb01264102c38f6ac06545
Rémi Bardon <remi@remibardon.name>
2026-07-06 17:28:03 +0200

    fix(core): Perform implicit `AND` when `QUERY`ing an ID

--- core/tests/loose_matching.rs
+++ core/tests/loose_matching.rs
@@ -15,7 +15,6 @@ use crate::common::*;
 fn test_search_is_case_insensitive_simple() {
     init_logging();
 
-    #[rustfmt::skip]
     let examples = [
         ("HeLLo", "hello"),
         ("ΚΌΣΜΟΣ", "κόσμος"),
@@ -43,7 +42,6 @@ fn test_search_is_case_insensitive_simple() {
 fn test_search_is_case_insensitive_proper() {
     init_logging();
 
-    #[rustfmt::skip]
     let examples = [
         ("ΚΌΣΜΟΣ", "κόσμοσ"),
         ("ΚΌΣΜΟΣ", "κόσμος"),
@@ -70,7 +68,6 @@ fn test_search_is_case_insensitive_proper() {
 fn test_search_is_unicode_normalized() {
     init_logging();
 
-    #[rustfmt::skip]
     let examples = [
         // Ligatures
         ("ﬃ", "ffi"),
@@ -100,7 +97,6 @@ fn test_search_is_unicode_normalized() {
 ///   v1.6.0 Sonic was storing non-normalized words in the FST.
 #[test]
 fn test_search_is_diacritics_insensitive() {
-    #[rustfmt::skip]
     test_ingest_then_query!(
         normalization_config: { diacritic_folding_enabled: true },
         search_config: { fuzzy_matching_enabled: false, prefix_matching_enabled: false },
@@ -111,7 +107,6 @@ fn test_search_is_diacritics_insensitive() {
     );
 
     // Example from <https://github.com/valeriansaliou/sonic/issues/245>.
-    #[rustfmt::skip]
     test_ingest_then_query!(
         normalization_config: { diacritic_folding_enabled: true },
         search_config: { fuzzy_matching_enabled: false, prefix_matching_enabled: true },
@@ -121,4 +116,47 @@ fn test_search_is_diacritics_insensitive() {
             ("Veronika S", true),
         ],
     );
+}
+
+#[test]
+fn test_no_fuzzy_matching_for_ids() {
+    init_logging();
+
+    let examples = [
+        // UUID.
+        (
+            vec![
+                "My user ID is \"6db14cb4-b82e-4e49-8016-ef76c4290a2f\"",
+                "My user ID is `6db14cb4-b82e-4e49-8016-ef76c4290a2e`",
+            ],
+            "6db14cb4-b82e-4e49-8016-ef76c4290a2f",
+        ),
+        // One term in the query (`abcd`) has only letters.
+        (
+            vec![
+                "My user ID is 6db14cb4-abcd-4e49-8016-ef76c4290a2e",
+                "My user ID is 6db14cb4-cdef-4e49-8016-ef76c4290a2e",
+            ],
+            "6db14cb4-abcd-4e49-8016-ef76c4290a2e",
+        ),
+        // Phone number like.
+        (
+            vec!["Here it is: 1234-567890-12", "Here it is: 1234-567890-13"],
+            "1234-567890-12",
+        ),
+    ];
+
+    for (example_idx, (messages, query)) in examples.into_iter().enumerate() {
+        let executor = make_test_executor_with_id(example_idx);
+
+        for (message_idx, &message) in messages.iter().enumerate() {
+            let id = &format!("chat:{message_idx}");
+            exec!(executor -> PUSH "messages" "user:1" id message);
+        }
+        exec!(executor -> TRIGGER consolidate);
+
+        let response = exec!(executor -> QUERY "messages" "user:1" query);
+
+        assert_eq!(response, ["chat:0"], "({messages:?}, {query:?})");
+    }
 }
