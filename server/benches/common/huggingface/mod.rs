9d102ebca3825ece81171226a616db374104fd7c
Baptiste Jamin <baptiste@crisp.chat>
2026-07-22 15:42:54 +0200

    bench(server): Add BEIR benchmark

--- server/benches/common/huggingface/mod.rs
+++ server/benches/common/huggingface/mod.rs
@@ -4,10 +4,6 @@
 // Copyright: 2026, Rémi Bardon <remi@remibardon.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
-mod download;
-mod load;
-mod wikipedia;
-
-pub use self::download::download_shards;
-pub use self::load::iter_shard;
-pub use self::wikipedia::WikipediaArticle;
+pub mod download;
+pub mod load;
+pub mod wikipedia;
--- server/tests/common/spawn_guard.rs
+++ server/tests/common/spawn_guard.rs
@@ -4,8 +4,32 @@
 // Copyright: 2026, Rémi Bardon <remi@remibardon.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
+use std::net::{SocketAddr, TcpStream};
+use std::time::{Duration, Instant};
+
 /// Automatically kills the child process on [Drop].
 pub struct SpawnGuard(pub std::process::Child);
+
+impl SpawnGuard {
+    /// Wait until the child listens on the expected address.
+    pub fn wait_until_ready(&mut self, addr: SocketAddr) {
+        let deadline = Instant::now() + Duration::from_secs(30);
+
+        loop {
+            if let Some(status) = self.try_wait().unwrap() {
+                panic!("Sonic exited with {status}");
+            }
+            if TcpStream::connect_timeout(&addr, Duration::from_millis(100)).is_ok() {
+                return;
+            }
+            assert!(
+                Instant::now() < deadline,
+                "Sonic did not listen on {addr} within 30 seconds"
+            );
+            std::thread::sleep(Duration::from_millis(50));
+        }
+    }
+}
 
 impl std::ops::Deref for SpawnGuard {
     type Target = std::process::Child;
